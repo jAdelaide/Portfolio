b00291a7da380ca21641e19a775e44818ba78120
jordanAdelaide <8adelaide.j@gmail.com>
2020-11-09 20:30:15 +0000

    Details page

---
+++ details.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <?php require 'templates/header.php'; ?>
+    <?php require 'languages.php'; ?>
+
+    <?php
+
+    $pageExists = FALSE;
+    foreach($languages as $language):
+        if($language['name'] == $_GET['id']): ?>
+            <h4 style="color: rgb(255, 177, 109); text-indent: 50px"><?php echo htmlspecialchars($_GET['id']); ?></h4>
+            <?php $pageExists = TRUE;
+        endif;
+    endforeach;
+    
+    if($pageExists == FALSE): ?>
+        <h2 style="color: rgb(255, 177, 109); text-align: center">There is no details page for "<?php echo htmlspecialchars($_GET['id']); ?>"</h2>
+    <?php endif; ?>
+
+    <?php require('templates/footer.php'); ?>
+    
+</html>
--- index.php
+++ index.php
@@ -14,16 +14,17 @@
 
 
 
-$languages = [
-    ['name' => 'Python', 'website' => 'https://www.python.org/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/120px-Python-logo-notext.svg.png'], // with text: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/486px-Python_logo_and_wordmark.svg.png'],
-    ['name' => 'Ruby', 'website' => 'https://www.ruby-lang.org/en/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/198px-Ruby_logo.svg.png'],
-    ['name' => 'HTML', 'website' => 'https://html.com/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png'],
-    ['name' => 'CSS', 'website' => 'https://sass-lang.com/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/512px-Sass_Logo_Color.svg.png'],
-    ['name' => 'PHP', 'website' => 'https://www.php.net/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/350px-Webysther_20160423_-_Elephpant.svg.png']
-];
+// $languages = [
+//     ['name' => 'Python', 'website' => 'https://www.python.org/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/120px-Python-logo-notext.svg.png'], // with text: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/486px-Python_logo_and_wordmark.svg.png'],
+//     ['name' => 'Ruby', 'website' => 'https://www.ruby-lang.org/en/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/198px-Ruby_logo.svg.png'],
+//     ['name' => 'HTML', 'website' => 'https://html.com/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png'],
+//     ['name' => 'CSS', 'website' => 'https://sass-lang.com/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/512px-Sass_Logo_Color.svg.png'],
+//     ['name' => 'PHP', 'website' => 'https://www.php.net/', 'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/350px-Webysther_20160423_-_Elephpant.svg.png']
+// ];
 ?>
 
 <?php require('templates/header.php'); ?>
+<?php require('languages.php'); ?>
 
 
 <h1 style="color: rgb(212, 32, 152)">Languages</h1>
@@ -41,6 +42,7 @@ $languages = [
               </a>
             </div>
             <div class="card-action center">
+                <a class="brand-text" href="details.php?id=<?php echo $language['name'] ?>">More info</a>
                 <a class="brand-text" href="<?php echo $language['name'] . '_projects.php'; ?>">My projects</a>
             </div>
           </div>
