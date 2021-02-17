defe5142968063fa1cc29863e17b034e3b1c235b
Jordan <8adelaide.j@gmail.com>
2021-02-17 20:39:20 +0000

    Title edits

--- sections/projectPages/timesheet.php
+++ sections/projectPages/timesheet.php
@@ -1,6 +1,6 @@
-<h2 style="text-align: center; color: rgb(180, 150, 140)">Employee Timesheet</h2>
+<h4 style="text-align: left; color: rgba(77, 191, 210, 0.85); margin-left: 50px; font-family: candara; font-weight: bold">Employee Timesheet</h4>
 
-<hr width="40%" color="rbg(3, 57, 82)">
+<hr width="40%" color="rbg(3, 57, 82)" style="margin-left: 40px">
 
 <p style="margin-left: 10px; margin-right: 10px; padding-left: 30px; padding-right: 30px; padding-top: 10px; padding-bottom: 10px; background-color: rgba(182, 169, 152, 0.15); border-radius: 30px; color: rgb(210, 190, 170); font-size: 17px; font-family: candara">
 I started working for this company along with 39 other interns and to keep a log of what we'd been doing, the company had made a functional timesheet in Microsoft Excel by utilising Microsoft's VBA code to create custom buttons and event driven functions. 
