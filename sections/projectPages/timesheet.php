3bf57ec19e70e628b86c2f5f6efd11e4bed9d310
Jordan <8adelaide.j@gmail.com>
2021-02-17 20:23:35 +0000

    Timesheet in projects section

---
+++ sections/projectPages/timesheet.php
@@ -0,0 +1,12 @@
+<h2 style="text-align: center; color: rgb(180, 150, 140)">Employee Timesheet</h2>
+
+<hr width="40%" color="rbg(3, 57, 82)">
+
+<p style="margin-left: 10px; margin-right: 10px; padding-left: 30px; padding-right: 30px; padding-top: 10px; padding-bottom: 10px; background-color: rgba(182, 169, 152, 0.15); border-radius: 30px; color: rgb(210, 190, 170); font-size: 17px; font-family: candara">
+I started working for this company along with 39 other interns and to keep a log of what we'd been doing, the company had made a functional timesheet in Microsoft Excel by utilising Microsoft's VBA code to create custom buttons and event driven functions. 
+They'd planned to send this out for everyone to fill in for themselves, however not all of the interns had Windows computers with Microsoft Office licenses so wouldn't be able to use it. 
+To deal with this, the company planned to recreate the timesheet on Google Sheets (Google's cloud based spreadsheet app that anyone can access online), however this is coded in Googles own Google Apps Script. 
+As everyone had lots of other things to do and no-one was familiar with GAS, this was put on hold for a couple of months until they decided to give the project to me to complete.
+<br></br>
+I was previously unaware that you could code in either Excel or the Google Apps but 
+</p>
