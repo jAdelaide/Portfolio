d1e573d51032b7e21c322313ffe4b6c13c11888b
Jordan <8adelaide.j@gmail.com>
2021-02-18 17:26:14 +0000

    Timesheet segment

--- sections/projectPages/timesheet.php
+++ sections/projectPages/timesheet.php
@@ -3,27 +3,14 @@
 <div style="margin-left: 10px; margin-right: 10px; padding-left: 30px; padding-right: 30px; padding-top: 10px; padding-bottom: 10px; background-color: rgba(182, 169, 152, 0.15); border-radius: 30px; color: rgb(210, 190, 170)">
     <p style="font-size: 17px; font-family: candara">
     This is a functional timesheet that I made for my current employer using Google Apps Script to add functionality to a Google Sheets spreadsheet. 
-    To see a functional version, you will need to follow the link below and make a copy of the spreadsheet and then refresh the page and then a new button should appear to the right of "Help" labeled "JORDAN". 
+    To see a functional version click the image below to open the timesheet, make a copy of the spreadsheet and then refresh the page and a new button should appear to the right of "Help" labeled "JORDAN". 
     Open the menu and upon selecting an option you will get an alert that it needs your permission to run as the script needs to read and write to the sheets. 
     If you want to see the code, once you've made a copy under "Tools" there will be an option labelled "Script editor" where you can see the code I used. 
-    <br><br>
     </p>
+    <br>
     
-    <!-- <div class="row" style="width: 100%">
-        <div class="column" style="width: 50%">
-            <a href="https://docs.google.com/spreadsheets/d/1KLvtX0E5XLsJzfkO9iCKFv_QAl3xg28GB7cfPIQF7Co/edit?usp=sharing" target="_blank">
-                <img src="https://raw.githubusercontent.com/jAdelaide/Portfolio/shove/assets/Timesheet.png" alt="Screenshot of the timesheet" title="Open the timesheet in a new tab" height="auto" width="50%" style="margin-left: 50%;">
-            </a>
-        </div>
-        <div class="column" style="width: 50%; margin-left: 50%">
-            <a href="https://docs.google.com/spreadsheets/d/1KLvtX0E5XLsJzfkO9iCKFv_QAl3xg28GB7cfPIQF7Co/edit?usp=sharing" target="_blank">
-                <img src="https://raw.githubusercontent.com/jAdelaide/Portfolio/shove/assets/Timesheet_dropdown.png" alt="Screenshot of the timesheet with the custom menu open" title="Open the timesheet in a new tab" height="auto" width="50%" style="margin-left: 50%;">
-            </a>
-        </div>
-    </div> -->
-
     <a href="https://docs.google.com/spreadsheets/d/1KLvtX0E5XLsJzfkO9iCKFv_QAl3xg28GB7cfPIQF7Co/edit?usp=sharing" target="_blank">
-        <img src="https://raw.githubusercontent.com/jAdelaide/Portfolio/shove/assets/Timesheet_joined.png" alt="Screenshot of the timesheet" title="Open the timesheet in a new tab" height="auto" width="100%">
+        <img src="https://raw.githubusercontent.com/jAdelaide/Portfolio/shove/assets/Timesheet_joined1.png" alt="Screenshot of the timesheet" title="Open the timesheet in a new tab" height="auto" width="100%">
     </a>
     <br><br>
     
