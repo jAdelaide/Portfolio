100b4c3523b759e6d68eee2199163e19ba04b390
Jordan <8adelaide.j@gmail.com>
2021-02-18 15:08:04 +0000

    Timesheet post

--- sections/projectPages/timesheet.php
+++ sections/projectPages/timesheet.php
@@ -4,6 +4,13 @@
 
 <p style="margin-left: 10px; margin-right: 10px; padding-left: 30px; padding-right: 30px; padding-top: 10px; padding-bottom: 10px; background-color: rgba(182, 169, 152, 0.15); border-radius: 30px; color: rgb(210, 190, 170); font-size: 17px; font-family: candara">
 This is a functional timesheet that I made for my current employer using Google Apps Script to add functionality to a Google Sheets spreadsheet. 
+To see a functional version, you will need to follow the link below and make a copy of the spreadsheet and then refresh the page and then a new button should appear to the right of "Help" labeled "JORDAN". 
+Open the menu and upon selecting an option you will get an alert that it needs your permission to run as the script needs to read and write to the sheets. 
+If you want to see the code, once you've made a copy under "Tools" there will be an option labelled "Script editor" where you can see the code I used. 
+<br><br>
 
 <a href="https://docs.google.com/spreadsheets/d/1KLvtX0E5XLsJzfkO9iCKFv_QAl3xg28GB7cfPIQF7Co/edit?usp=sharing" target="_blank">Timesheet</a>
+<br><br>
+
+
 </p>
