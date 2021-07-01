f7cf250218c4881c5dc0386b2dd1363dac49fced
Jordan <8adelaide.j@gmail.com>
2021-07-01 18:40:42 +0100

    Adding indent to left of project buttons

--- sections/projects.php
+++ sections/projects.php
@@ -4,8 +4,9 @@
 <h1 style="color: rgb(212, 32, 152); text-indent: 50px; font-family: 'Luckiest Guy', cursive; font-size: 40px">Projects</h1>
 <hr align="left" width="1000px" color=" rgb(0, 0, 0,)">
 
-
-<?php include 'projectPages/FSx.php'; ?>
-<?php include 'projectPages/azureVM.php'; ?>
-<?php include 'projectPages/timesheet.php'; ?> 
-<?php include 'projectPages/vbsParams.php'; ?> 
+<div style="padding-left: 50px">
+    <?php include 'projectPages/FSx.php'; ?>
+    <?php include 'projectPages/azureVM.php'; ?>
+    <?php include 'projectPages/timesheet.php'; ?> 
+    <?php include 'projectPages/vbsParams.php'; ?> 
+</div>
