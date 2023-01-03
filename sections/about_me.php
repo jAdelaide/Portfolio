f06bb2f890bf5dd5b116f93b7cc242ad82bdfc55
Jordan <8adelaide.j@gmail.com>
2023-01-03 14:51:11 +0000

    Minor code formatting edits

--- sections/about_me.php
+++ sections/about_me.php
@@ -6,11 +6,10 @@
 
 <div class="row">
     <div class="column" style="width: 90%">
-        <div style="float: left; z-index: 0; position: relative; margin-left: 30px; margin-right: 250px; margin-top: 10px; margin-bottom: 10px; padding-left: 30px; padding-right: 150px; padding-top: 3px; padding-bottom: 10px; background-color: rgba(222, 169, 202, 0.15); border-radius: 200px 1000px 1000px 200px; color: rgb(210, 190, 170); font-size: 19px; font-family: candara;">
+        <div style="float: left; position: relative; margin-left: 30px; margin-right: 250px; margin-top: 10px; margin-bottom: 10px; padding-left: 30px; padding-right: 150px; padding-top: 3px; padding-bottom: 10px; border-radius: 200px 1000px 1000px 200px; color: rgb(210, 190, 170); background-color: rgba(222, 169, 202, 0.15); font-size: 19px; font-family: candara;">
             <p style="position: relative; z-index: 0">
-            Hey! I'm Jordan, an enthusiastic and skilled trainee developer that loves to make things.
-            My background is in mechanical engineering and design and I only started coding in 2020, but have quickly gained experience in a number of 
-            areas - including enough PHP, HTML and CSS to build this portfolio website from scratch. 
+            Hey! I'm Jordan, an enthusiastic and skilled developer in training that loves to make things.
+            My background is in mechanical engineering and design and I only started coding in 2020, but have quickly gained experience in a number of areas - including enough PHP, HTML and CSS to build this portfolio website from scratch. 
             </p>
             <div style="text-align: center; z-index: 0">
                 <span class="dot" style="height: 25px; width: 10px; background-color: rgba(222, 169, 202, 0.15); border-radius: 50%; display: inline-block"></span>
@@ -20,14 +19,12 @@
                 <span class="dot" style="height: 25px; width: 10px; background-color: rgba(222, 169, 202, 0.15); border-radius: 50%; display: inline-block"></span>
             </div>
             <p style="position: relative; z-index: 0">
-                I have a huge range of interests and love to learn and try new things. No matter if I'm doing front-end programming; writing a script to 
-                automate a process; making or designing a physical or CAD product; painting a picture or anything else, I am motivated by and enjoy producing
-                things that I would enjoy as a user. 
+            I have a huge range of interests and love to learn and try new things. No matter if I'm doing front-end programming; writing a script to automate a process; making or designing a physical or CAD product; painting a picture or anything else, I am motivated by and enjoy producing things that I would enjoy as a user.
             </p>
         </div>
     </div>
     <div class="column" style="width: 10%">
-        <div style="z-index: 1; position: absolute">
+        <div style="position: absolute">
             <form style="margin-top: -30px; margin-left: -90%">
                 <img src="../assets/jordanBear.png" height="450px" title="That's me!!!" alt="A picture of me in a pile of giant teddy bears">
             </form>
