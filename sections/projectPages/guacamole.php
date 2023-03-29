d68563fa02767667b82b62b2508f61b2054e6a77
Jordan <8adelaide.j@gmail.com>
2023-03-29 08:18:06 +0100

    Updated to open in new tab

--- sections/projectPages/guacamole.php
+++ sections/projectPages/guacamole.php
@@ -1,3 +1,3 @@
-<a href="https://jadelaide.github.io/Guac-a-mole/guacAmole.html"</a>
+<a href="https://jadelaide.github.io/Guac-a-mole/guacAmole.html" target="_blank"</a>
     <img src="https://raw.githubusercontent.com/jAdelaide/Guac-a-mole/main/moleButton.png" height="90px" title="Guacamole game" alt="A jumping cartoon mole" style="margin-top: -5px; margin-bottom: -35px; margin-left: 8px; margin-right: 12px">
 </a>
--- sections/projectPages/jordasaurusJump.php
+++ sections/projectPages/jordasaurusJump.php
@@ -1,3 +1,3 @@
-<a href="https://jordasaurusjump.jordanadelaide.repl.co/"</a>
+<a href="https://jordasaurusjump.jordanadelaide.repl.co/" target="_blank"</a>
     <img src="../../assets/Jordasaur.png" height="90px" title="Jordasaurus Jump game" alt="A colourful 8-bit dinosaur" style="margin-top: -10px; margin-bottom: -30px; margin-left: 12px; margin-right: 12px">
 </a> 
